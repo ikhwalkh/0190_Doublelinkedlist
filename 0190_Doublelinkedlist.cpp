93554c372fd67fe7d96b0fa83580b9b754123ba1
ikhwalkh <203380321+ikhwalkh@users.noreply.github.com>
2026-05-01 15:18:56 +0700

    void traverse

--- 0190_Doublelinkedlist.cpp
+++ 0190_Doublelinkedlist.cpp
@@ -131,3 +131,26 @@ public:
         delete current;
         cout << "Record with roll number " << rollNo << " deleted" << endl;
     }
+
+     void traverse()
+    {
+        if (START == NULL)
+        {
+            cout << "\nList is empty" << endl;
+            return;
+        }
+
+        // Step 1: Start from first node as currentnode
+        Node *currentnode = START;
+
+        cout << "\nRecords in ascending order of roll number are:\n";
+        while (currentnode != NULL)
+        {
+            // Step 2: Print roll no as currentnode->info
+            cout << currentnode->no << " ";
+
+            // Step 3: Move to next node
+            currentnode = currentnode->next;
+        }
+        cout << endl;
+    }
