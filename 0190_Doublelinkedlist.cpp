6fd46f7c9976b13ee09b3f011013ee47da61cc32
ikhwalkh <203380321+ikhwalkh@users.noreply.github.com>
2026-05-01 15:14:43 +0700

    void addNode

--- 0190_Doublelinkedlist.cpp
+++ 0190_Doublelinkedlist.cpp
@@ -23,3 +23,39 @@ public:
     {
         START = NULL;
     }
+
+     void addNode()
+    {
+        int nim;
+        cout << "Enter the roll number of the student: ";
+        cin >> nim;
+
+        // Step 1: Allocate memory for new node
+        Node *newnode = new Node();
+
+        // Step 2: Assign value to the data field
+        newnode->no = nim;
+
+        // If list is empty or inserting at 1st is empty or not in sorted
+        if (START == NULL || nim <= START->no)
+        {
+            if (START != NULL && nim == START->no)
+            {
+                cout << "\nDuplicate roll numbers not allowed" << endl;
+                return;
+            }
+
+            // Step 3: newnode.next = START
+            newnode->next = START;
+
+            // Step 4: START.prev = newnode (if START exists)
+            if (START != NULL)
+                START->prev = newnode;
+
+            // Step 5: newnode.prev = NULL
+            newnode->prev = NULL;
+
+            // Step 6: START = newnode
+            START = newnode;
+            return;
+        }
