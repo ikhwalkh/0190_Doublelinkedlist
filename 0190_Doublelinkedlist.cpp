35db3ec8f74777b6e806ccd2ab2cc755ae77df67
ikhwalkh <203380321+ikhwalkh@users.noreply.github.com>
2026-05-01 15:18:20 +0700

    if node, link next node to prev of current, and delete node

--- 0190_Doublelinkedlist.cpp
+++ 0190_Doublelinkedlist.cpp
@@ -109,3 +109,25 @@ public:
             cout << "Record not found" << endl;
             return;
         }
+
+          // Step 2: If node is at the beginning
+        if (current == START)
+        {
+            START = current->next; // Step 3a
+            if (START != NULL)
+                START->prev = NULL; // Step 3b
+        }
+        else
+        {
+            // Step 4: Link previous node to next of current
+            current->prev->next = current->next;
+
+            // Step 5: Link next node to prev of current
+            if (current->next != NULL)
+                current->next->prev = current->prev;
+        }
+
+        // Step 6: Delete node
+        delete current;
+        cout << "Record with roll number " << rollNo << " deleted" << endl;
+    }
